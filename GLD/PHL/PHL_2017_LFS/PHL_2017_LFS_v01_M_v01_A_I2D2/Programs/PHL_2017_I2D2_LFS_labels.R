6d42e8dcdc73f4618e864a18a06cff54b81135c0
Tom Mosher <56852146+buscandoaverroes@users.noreply.github.com>
2021-06-16 16:08:33 -0400

    create labels.R

---
+++ GLD/PHL/PHL_2017_LFS/PHL_2017_LFS_v01_M_v01_A_I2D2/Programs/PHL_2017_I2D2_LFS_labels.R
@@ -0,0 +1,6 @@
+# PHL_2017_I2D2_LFS_labels.R
+# A helper script for PHL_2017_I2D2_LFS_labels.do that harmonizes data labels
+
+# Note, this script assumes that you have already run PHL_2017_I2D2_LFS_labels.do and output an iecodebook xlsx file 
+# and duplicated it manually and saved it under the suffix "-IN.xlsx". For more info, see README
+
