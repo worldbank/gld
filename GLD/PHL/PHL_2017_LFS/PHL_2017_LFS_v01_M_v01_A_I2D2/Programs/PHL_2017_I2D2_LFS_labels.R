0d3b7457f092f12dc56e4b27e670d11c22e6a640
Tom Mosher <56852146+buscandoaverroes@users.noreply.github.com>
2021-06-16 16:36:35 -0400

    Update PHL_2017_I2D2_LFS_labels.R

--- GLD/PHL/PHL_2017_LFS/PHL_2017_LFS_v01_M_v01_A_I2D2/Programs/PHL_2017_I2D2_LFS_labels.R
+++ GLD/PHL/PHL_2017_LFS/PHL_2017_LFS_v01_M_v01_A_I2D2/Programs/PHL_2017_I2D2_LFS_labels.R
@@ -4,3 +4,68 @@
 # Note, this script assumes that you have already run PHL_2017_I2D2_LFS_labels.do and output an iecodebook xlsx file 
 # and duplicated it manually and saved it under the suffix "-IN.xlsx". For more info, see README
 
+library(tidyverse)
+library(openxlsx)
+library(readxl)
+
+setwd("Y:/GLD-Harmonization/551206_TM/PHL/PHL_2012_LFS/PHL_2012_LFS_v01_M_v01_A_I2D2/Doc")
+
+# Value Lable Harmonization ----
+## Read in all choices ----
+round1 <- read_xlsx(
+  path = "PHL_2012_append_template-IN-R.xlsx",
+  sheet = "choices_JAN2012",
+  col_names = TRUE
+)
+
+round2 <- read_xlsx(
+  path = "PHL_2012_append_template-IN-R.xlsx",
+  sheet = "choices_APR2012",
+  col_names = TRUE
+)
+
+round3 <- read_xlsx(
+  path = "PHL_2012_append_template-IN-R.xlsx",
+  sheet = "choices_JUL2012",
+  col_names = TRUE
+)
+
+round4 <- read_xlsx(
+  path = "PHL_2012_append_template-IN-R.xlsx",
+  sheet = "choices_OCT2012",
+  col_names = TRUE
+)
+
+
+
+## bind rows, keep distinct values ----
+labs <- bind_rows(round1, round2, round3, round4) %>%
+  distinct(
+    list_name, value,     # determine duplicates only across these two varaibles
+    .keep_all = TRUE      # keep the third variable 
+    )
+
+
+
+
+# Write the object to iecodebook output ----
+## read in xlsx for editing ----
+template <- loadWorkbook("PHL_2012_append_template-IN-S.xlsx")
+
+
+## write data----
+writeData(
+  wb = template, 
+  sheet = "choices",
+  x = labs, 
+  xy= c(1,1),
+  colNames = TRUE # the names need to be there for iecodebook to work.
+  
+)
+
+
+## save / export workbook ----
+saveWorkbook(template, 
+             file = "PHL_2012_append_template-IN-S.xlsx",
+             overwrite = TRUE)
+
