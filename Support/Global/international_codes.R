4740d218cc460892e257c2683f41e2985127b1c2
Tom <56852146+buscandoaverroes@users.noreply.github.com>
2021-08-03 08:17:48 -0400

    test pdf import

--- Support/Global/international_codes.R
+++ Support/Global/international_codes.R
@@ -13,10 +13,16 @@
 # Most of the findings here will be docuements in "Industry_Occupation Codes.md" in 
 # the "Country Survey Details" Folder for the Philippines
 
+library(tidyverse)
 library(stringr)
+library(pdftools)
 
 
 load(PHL_meta)
+load(PHL_labels)
+
+psic_path <- file.path(PHL, "PHL_docs/International Codes/PSA_PSIC_2009.pdf")
+
 
 
 
@@ -26,3 +32,10 @@ load(PHL_meta)
 # Import ISIC 3.0 data --------------------------------- 
 # we know that only years 1997 - 2011 use
 isic3 <- read_delim(file = file.path(PHL, "PHL_data/GLD/international_codes/ISIC_Rev_3_english_structure.txt"))
+
+
+# pdf import 
+psic09_info <- pdf_info(psic_path)
+psic09_data <- pdf_data(psic_path)
+psic09_toc  <- pdf_toc(psic_path)
+psic09_text <- pdf_text(psic_path)
