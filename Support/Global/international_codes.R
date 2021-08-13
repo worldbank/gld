6fa2827807a23305384f1af66e4223cb269465ae
Tom <56852146+buscandoaverroes@users.noreply.github.com>
2021-08-13 17:34:49 -0400

    replace with 09

--- Support/Global/international_codes.R
+++ Support/Global/international_codes.R
@@ -28,7 +28,8 @@ if (FALSE) {load(PHL_labels)}
 source(file.path(code, "Global/read_pdf_table.R"))
 
 # pdf file path
-psic_path <- file.path(PHL, "PHL_docs/International Codes/PSA_PSIC_2009.pdf")
+psic94_path <- file.path(PHL, "PHL_docs/International Codes/PSA_PSIC_1994.pdf")
+psic09_path <- file.path(PHL, "PHL_docs/International Codes/PSA_PSIC_2009.pdf")
 psoc_path <- file.path(PHL, "PHL_docs/International Codes/PSA_PSOC_2012.pdf")
 
 
@@ -41,10 +42,98 @@ psoc_path <- file.path(PHL, "PHL_docs/International Codes/PSA_PSOC_2012.pdf")
 # we know that only years 1997 - 2011 use this
 UNisic3 <- read_delim(file = file.path(PHL, "PHL_data/GLD/international_codes/ISIC_Rev_3_english_structure.txt"))
 
-## ISIC Raw ----
-isic_codes_raw <- read_pdf(
+
+pdf <- pdftools::pdf_data(psic94_path)
+pdf[[122]] %>% View()
+
+## ISIC 94 Raw ----
+isic94_codes_raw <- read_pdf(
+  
+  pdf_path = psic94_path,
+  page_min = 22,
+  page_max = 316,
+  varnames = c("class", "subclass", "psic1994", "isic4", "acic"),
+  ymin = 90,
+  xlabel = c(155, 420),
+  xmin = c(91, 131, 415, 446, 501),
+  xmax = c(130, 175, 445, 500, 9999)
+)
+
+
+
+
+# cleaning ----
+## setup 
+table_vars_gc <- c("group", "class")
+table_vars_spia<- c("psic1994", "isic4", "acic")
+rowAny <- function(x) rowSums(x) > 0 
+
+
+## create "class" from subclass and "group" variable from class
+## we know that class is always the first four digits of subclass 
+## and group is always the first 3 digits of class. But for some 
+## obs, class is provided, so do not overwrite this info. Treat 
+## given info as authoritative.
+
+isic94_codes <- isic94_codes_raw %>%
+  mutate(
+    class = case_when(is.na(class)  ~ str_sub(subclass, 1,4),
+                      TRUE          ~ class),
+    group = str_sub(class, 1,3)) %>%
+  select(y, page_grp, page, group, class, everything())
+
+
+
+# this object has "leftotver stubs" that do not list any useful information.
+# it only lists the structure of the group and class, which can be extracted so
+# create a leftover object to verify that we filtered correctly
+# object 1 is what we want removed where both group and class are missing.
+# object 2 is where all of psic1994, isic4, acic vars are missing
+
+isic94_leftover1 <-  isic94_codes %>% 
+  filter(across(all_of(table_vars_gc), ~ is.na(.x))) 
+
+isic94_leftover2 <- isic94_codes %>%
+  filter(across(all_of(table_vars_spia), ~is.na(.x)))
+
+isic94_leftover <- bind_rows(isic94_leftover1, isic94_leftover2)
+
+
+
+## ISIC clean ----
+isic94_clean <- isic94_codes %>%
+  ## eliminate the "("
+  mutate(psic1994 = str_replace(psic1994, "\\(", "")) %>%
+  mutate(psic1994 = str_replace(psic1994, "\\)", "")) %>%
+  ## eliminate group and class-only rows
+  #filter(rowAny(across(table_vars_gc, ~ !is.na(.x)))) %>% # at least 1 col must be non-NA
+  filter(rowAny(across(table_vars_spia, ~ !is.na(.x)))) %>% # at least 1 col must be non-NA
+  ungroup() %>%
+  select(-y, -text, -page_grp)
+
+# check
+assertthat::assert_that( (nrow(isic94_clean) + nrow(isic94_leftover2)) == nrow(isic94_codes)   )
+
+
+isic94_clean %>% janitor::get_dupes() # there is 1 pair of dups
+
+isic94_clean <- isic94_clean %>%
+  distinct()
+
+
+assertthat::assert_that( sum(str_length(isic94_clean$class) <= 3, na.rm = TRUE) ==0 ) # should be 0 or close to
+assertthat::assert_that( sum(str_length(isic94_clean$group) != 3, na.rm = TRUE) == 0 ) # should be 0 or close to
+
+
+
+
+
+
+
+## ISIC 09 Raw ----
+isic09_codes_raw <- read_pdf(
                       
-    pdf_path = psic_path,
+    pdf_path = psic09_path,
     page_min = 22,
     page_max = 316,
     varnames = c("class", "subclass", "psic1994", "isic4", "acic"),
@@ -70,7 +159,7 @@ rowAny <- function(x) rowSums(x) > 0
 ## obs, class is provided, so do not overwrite this info. Treat 
 ## given info as authoritative.
 
-isic_codes <- isic_codes_raw %>%
+isic09_codes <- isic09_codes_raw %>%
   mutate(
     class = case_when(is.na(class)  ~ str_sub(subclass, 1,4),
                       TRUE          ~ class),
@@ -85,18 +174,18 @@ isic_codes <- isic_codes_raw %>%
 # object 1 is what we want removed where both group and class are missing.
 # object 2 is where all of psic1994, isic4, acic vars are missing
 
-isic_leftover1 <-  isic_codes %>% 
+isic09_leftover1 <-  isic09_codes %>% 
   filter(across(all_of(table_vars_gc), ~ is.na(.x))) 
 
-isic_leftover2 <- isic_codes %>%
+isic09_leftover2 <- isic09_codes %>%
   filter(across(all_of(table_vars_spia), ~is.na(.x)))
 
-isic_leftover <- bind_rows(isic_leftover1, isic_leftover2)
+isic09_leftover <- bind_rows(isic09_leftover1, isic09_leftover2)
 
 
 
 ## ISIC clean ----
-isic_clean <- isic_codes %>%
+isic09_clean <- isic09_codes %>%
   ## eliminate the "("
   mutate(psic1994 = str_replace(psic1994, "\\(", "")) %>%
   mutate(psic1994 = str_replace(psic1994, "\\)", "")) %>%
@@ -107,17 +196,17 @@ isic_clean <- isic_codes %>%
   select(-y, -text, -page_grp)
   
 # check
-assertthat::assert_that( (nrow(isic_clean) + nrow(isic_leftover2)) == nrow(isic_codes)   )
+assertthat::assert_that( (nrow(isic09_clean) + nrow(isic09_leftover2)) == nrow(isic09_codes)   )
 
 
-isic_clean %>% janitor::get_dupes() # there is 1 pair of dups
+isic09_clean %>% janitor::get_dupes() # there is 1 pair of dups
 
-isic_clean <- isic_clean %>%
+isic09_clean <- isic09_clean %>%
   distinct()
 
 
-assertthat::assert_that( sum(str_length(isic_clean$class) <= 3, na.rm = TRUE) ==0 ) # should be 0 or close to
-assertthat::assert_that( sum(str_length(isic_clean$group) != 3, na.rm = TRUE) == 0 ) # should be 0 or close to
+assertthat::assert_that( sum(str_length(isic09_clean$class) <= 3, na.rm = TRUE) ==0 ) # should be 0 or close to
+assertthat::assert_that( sum(str_length(isic09_clean$group) != 3, na.rm = TRUE) == 0 ) # should be 0 or close to
 
 
 
@@ -137,7 +226,7 @@ assertthat::assert_that( sum(str_length(isic_clean$group) != 3, na.rm = TRUE) ==
 # Use Function to import raw data 
 # ISCO raw ----
 
-psoc_codes_raw <- read_pdf(
+psoc09_codes_raw <- read_pdf(
   
       pdf_path = psoc_path,
       page_min = 102,
@@ -166,7 +255,7 @@ isco_order <- c("submajor", "minor", "unit", "psoc92", "isco08")
 ## obs, this info is provided, so do not overwrite this info. Treat 
 ## given info as authoritative.
 
-isco_codes <- psoc_codes_raw %>%
+psoc09_codes <- psoc09_codes_raw %>%
   mutate(
     minor    = case_when(is.na(minor)  ~ str_sub(unit, 1,4),
                       TRUE          ~ minor),
@@ -176,28 +265,28 @@ isco_codes <- psoc_codes_raw %>%
 
 
 # filter out leftoverstubs 
-isco_leftover1 <-  isco_codes %>% 
+isco09_leftover1 <-  psoc09_codes %>% 
   filter(across(all_of(table_vars_sm), ~ is.na(.x))) 
 
-isco_leftover2 <- isco_codes %>%
+isco09_leftover2 <- psoc09_codes %>%
   filter(across(all_of(table_vars_pi), ~is.na(.x)))
 
-isco_leftover <- bind_rows(isco_leftover1, isco_leftover2)
+isco09_leftover <- bind_rows(isco09_leftover1, isco09_leftover2)
 
 
 
 # ISCO clean ----
 ## note that for now in order to sidestep issue #96, will not filter yet
-isco_clean <- isco_codes %>%
+isco09_clean <- psoc09_codes %>%
   ## eliminate group and class-only rows
   #filter(rowAny(across(table_vars_sm, ~ !is.na(.x)))) %>% # at least 1 col must be non-NA
   filter(rowAny(across(table_vars_pi, ~ !is.na(.x)))) # at least 1 col must be non-NA
   
   # check
-  assertthat::assert_that( (nrow(isco_clean) + nrow(isco_leftover2)) == nrow(isco_codes)   )
+  assertthat::assert_that( (nrow(isco09_clean) + nrow(isco09_leftover2)) == nrow(psoc09_codes)   )
 
   ## eliminate strange puncutation marks
-  isco_clean <- isco_clean %>%
+  isco09_clean <- isco09_clean %>%
     mutate(psoc92 = str_replace(psoc92, "[:punct:]", "")) %>%
     mutate(psoc92 = str_replace(psoc92, "p", "")) %>% 
     mutate(psoc92 = str_replace(psoc92, "\\`", NA_character_)) %>% 
@@ -206,13 +295,13 @@ isco_clean <- isco_codes %>%
     select(-y, -page_grp) 
 
 # clean duplicates
-  isco_clean %>% janitor::get_dupes() # there is 1 pair of dups
+  isco09_clean %>% janitor::get_dupes() # there is 1 pair of dups
   
-  isco_clean <- isco_clean %>%
+  isco09_clean <- isco09_clean %>%
     distinct()
 
 
-assertthat::assert_that( sum(str_length(isco_clean$submajor) != 3, na.rm=TRUE) == 0 ) # should be 0 or close to
+assertthat::assert_that( sum(str_length(isco09_clean$submajor) != 3, na.rm=TRUE) == 0 ) # should be 0 or close to
 
 
 
@@ -221,17 +310,17 @@ assertthat::assert_that( sum(str_length(isco_clean$submajor) != 3, na.rm=TRUE) =
 # save data ----
 
 # Rdata 
-save(isic_codes_raw, isic_codes, isic_leftover, isic_clean, psic_path, 
-     psoc_codes_raw, isco_codes, isco_leftover, isco_clean, psoc_path,
+save(isic09_codes_raw, isic09_codes, isic09_leftover, isic09_clean, psic09_path, 
+     psoc09_codes_raw, psoc09_codes, isco09_leftover, isco09_clean, psoc_path,
      read_pdf, UNisic3,
      file = file.path(PHL, "PHL_data/international_codes.Rdata") )
 
 
 # export dta 
-haven::write_dta(isic_clean,
+haven::write_dta(isic09_clean,
                  path = file.path(PHL, "PHL_data/GLD/PHL_PSIC_ISIC_key.dta"),
                  version = 14)
 
-haven::write_dta(isco_clean,
+haven::write_dta(isco09_clean,
                  path = file.path(PHL, "PHL_data/GLD/PHL_PSOC_ISCO_key.dta"),
                  version = 14)
