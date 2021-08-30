300c5cb83efa265095ccce9e8439afbd6d2c94ef
Tom <56852146+buscandoaverroes@users.noreply.github.com>
2021-08-30 16:38:19 -0400

    skip as.symbol

--- Support/Global/best_matches.R
+++ Support/Global/best_matches.R
@@ -1,6 +1,6 @@
-#' Determines most likely matches between two vectors of numeric classification codes.
+#' Determines most likely matches between two vectors of numer international_code  classif international_code ation codes.
 #' @param df The tibble or data.frame object containing the two columns of data to match
-#' @param country_code The name of the column that contains the country-specific code
+#' @param country_code The name of the column that contains the country-specif international_code  code
 #' @param international_code The name of the column that contains the international code
 #' @return a 3-element list object that contains the final match tibble, a results tibble, and a ggplot.
 
@@ -11,8 +11,8 @@ best_match <- function(df,
   
   # 1. Load and Reduce df
   # determine colnames + symbols
-  cc <- as.symbol(country_code )
-  ic <- as.symbol(international_code)
+   # country_code  <- as.symbol(country_code )
+   # international_code  <- as.symbol(international_code)
   vars <- c(as.character(country_code), as.character(international_code))
   
   # Drop columns we are not interest in, drop rows where int code is missing
@@ -30,9 +30,9 @@ best_match <- function(df,
   # Step 2 - Match at 4 digits ----------
   # Match if concordance is 100%
   match_1 <- df %>%
-    count({{cc}}, {{ic}}) %>%
+    count({{ country_code }}, {{ international_code }}) %>%
     rename(instance = n) %>%
-    group_by({{cc}}) %>%
+    group_by({{ country_code }}) %>%
     mutate(sum = sum(instance)) %>%
     ungroup() %>%
     mutate(pct = round((instance/sum)*100,1)) %>%
@@ -48,16 +48,16 @@ best_match <- function(df,
 
  # Reduce df to cases not yet matched
  df_2 <- df %>%
-   filter(!({{cc}} %in% match_1[[country_code]]))
+   filter(!({{ country_code }} %in% match_1[[country_code]]))
 
- # Reduce ISIC codes to three digits
+ # Reduce IS international_code  codes to three digits
  df_2[[international_code]] <- substr(df_2[[international_code]],1,3)
 
  # Match if perfect
  match_2 <- df_2 %>% 
-   count({{cc}}, {{ic}}) %>% 
+   count({{ country_code }}, {{ international_code }}) %>% 
    rename(instance = n) %>%
-   group_by({{cc}}) %>%
+   group_by({{ country_code }}) %>%
    mutate(sum = sum(instance)) %>%
    ungroup() %>%
    mutate(pct = round((instance/sum)*100,1)) %>%
@@ -75,31 +75,31 @@ best_match <- function(df,
 
 # Reduce df to cases not yet matched
 df_3 <- df_2 %>%
-  filter(!({{cc}} %in% match_2[[country_code]]))
+  filter(!({{ country_code }} %in% match_2[[country_code]]))
 
 
-  # Reduce ISIC code to two digits
+  # Reduce IS international_code  code to two digits
   df_3[[international_code]] <- substr(df_3[[international_code]],1,2)
   
-  # Match by maximum, account for cases where df_3 may be null
+  # Match by maximum, a country_code ount for cases where df_3 may be null
   if (dim(df_3)[1] > 0) {
     set.seed(61035)
     match_3 <- df_3 %>% 
-      count({{cc}}, {{ic}}) %>% 
+      count({{ country_code }}, {{ international_code }}) %>% 
       rename(instance = n) %>%
-      group_by({{cc}}) %>%
+      group_by({{ country_code }}) %>%
       mutate(sum = sum(instance)) %>%
       ungroup() %>%
       mutate(pct = round((instance/sum)*100,1))
-    group_by({{cc}}) %>%
+    group_by({{ country_code }}) %>%
       slice_max(pct) %>%
       sample_n(1)
   } else {
     set.seed(61035)
     match_3 <- df_3 %>% 
-      count({{cc}}, {{ic}}) %>% 
+      count({{ country_code }}, {{ international_code }}) %>% 
       rename(instance = n) %>%
-      group_by({{cc}}) %>%
+      group_by({{ country_code }}) %>%
       mutate(sum = sum(instance)) %>%
       ungroup() %>%
       mutate(pct = round((instance/sum)*100,1))
@@ -120,10 +120,10 @@ df_3 <- df_2 %>%
 
 
 concord <- bind_rows(match_1, match_2, match_3) %>%
-  select( {{cc}}, {{ic}}, pct) %>%
+  select( {{ country_code }}, {{ international_code }}, pct) %>%
   rename(match = pct) %>%
-   mutate( "{{cc}}" := str_pad({{cc}}, 4, pad = "0", side = "right"),
-           "{{ic}}" := str_pad({{ic}}, 4, pad = "0", side = "right"))
+   mutate( "{{ country_code }}" := str_pad({{ country_code }}, 4, pad = "0", side = "right"),
+           "{{ international_code }}" := str_pad({{ international_code }}, 4, pad = "0", side = "right"))
 
 results <- tibble(
   match_no = c(1,2,3),
