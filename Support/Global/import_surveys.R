6d547f0f4bd689e66d7e42f22755e33926675d19
Tom <56852146+buscandoaverroes@users.noreply.github.com>
2021-09-14 14:57:38 -0400

    Create import_surveys.R

---
+++ Support/Global/import_surveys.R
@@ -0,0 +1,43 @@
+#import_surveys.R
+#  library(tidyverse)
+
+import_surveys <- function(eval_directory, 
+                           vars = NULL,
+                           version = "v01_A",
+                           file.pattern = paste0("\\", version, "_GLD_ALL.dta$")
+                           ) {
+  
+  
+  
+  files <- list.files(eval_directory,     
+                      pattern = file_pattern, 
+                      recursive = TRUE,   # search all sub folders
+                      full.names = TRUE,  # list full file names
+                      include.dirs = TRUE) %>% # include the full file path
+    as_tibble() %>%
+    rename(paths = value) %>%
+    mutate(  
+      # assume the filename is the 4-digit numeric value after the final "/"
+      names = stringr::str_extract(basename(paths), "[:digit:]{4}")
+    )
+  
+  # key variables
+  variables <- c("countrycode", "year", "hhid", "pid")
+  if (!is.null(vars)) {
+    variables <- c(variables, vars)
+  }
+  
+  # start with empty list
+  file_list <- list()
+  
+  
+  df <- map2(files$names, files$paths, 
+             function(x, y) file_list[[x]] <<- haven::read_dta(y) %>%
+               select(any_of(variables))
+  )
+  
+  
+  df <- bind_rows(df) 
+  
+  return(df)
+}
