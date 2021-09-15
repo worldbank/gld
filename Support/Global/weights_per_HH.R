c381a716300f0d59bad06fa4b7edfb3342cc5b8d
Tom <56852146+buscandoaverroes@users.noreply.github.com>
2021-09-15 13:44:04 -0400

    non-working population

--- Support/Global/weights_per_HH.R
+++ Support/Global/weights_per_HH.R
@@ -2,16 +2,20 @@
 # assume you run main.R
 
 library(tidyverse)
+library(survey)
 
 
 # import all surveys 
 ## first, use the import function 
 source(file.path(code,"Global/import_surveys.R"))
 
+## maybe, load data
+if (TRUE) {load(file.path(PHL, "PHL_data/GLD/weights.Rdata"))}
+  
 
 ## import using function
 test <- import_surveys(eval_directory = PHL, vars = "weight") # this takes 15 min or so
-
+#test <- test %>% filter(!is.na(weight))
 
 ## create a difference variable, TRUE means weight is same in each household
 sum <- test %>% 
@@ -30,7 +34,30 @@ sum_yr <- sum %>%
   group_by(countrycode, year) %>% 
   summarise(
     pct_wgt_same = sum(wgt_dif)/n()
-  ) 
+  )
+
+sum_pop <- test %>%
+  ungroup() %>%
+  mutate(one = 1) %>%
+  group_by(countrycode, year) %>%
+  summarise(
+    one          = mean(one),
+    population   = weighted.mean(one, weight)
+  ) %>%
+  select(-one)
+
+
+phl97 <- test %>% filter(year == 1997) %>% mutate(one = 1)
+
+weighted.mean(phl97$one, phl97$weight)
+
+## expanded population 
+
+svy <- svydesign(ids = ~countrycode + year + hhid + pid ,
+                 weights = ~weight, 
+                 data = test) ## wont' run, missing values in weights?
+
+summary(svy)
 
 
 # save 
