3bf4a355490ea968904a0b81887f4e7b6c2dedb6
gronert-m <mario.gronert@gmail.com>
2023-01-11 11:18:50 +0000

    Update R code

--- Support/B - Country Survey Details/BRA/PNAD/utilities/Brazil_currency_changes.R
+++ Support/B - Country Survey Details/BRA/PNAD/utilities/Brazil_currency_changes.R
@@ -1,6 +1,11 @@
 
+# The object "df" we start with is the result of appending the GLD files for all years, not shown here.
 
-wage <- 
+
+# Step 1 - Create relevant data sets --------------------------------------
+
+# Create mean wage data from 1981 to 1998 for employees with wage info, as is (pure nominal values)
+wage_as_is <- 
   df %>% 
   filter(empstat == 1) %>%
   filter(!is.na(wage_no_compen)) %>%
@@ -10,17 +15,54 @@ wage <-
     mean_employee_wage = round(mean(wage_no_compen), 0)
 )
 
-View(wage)
-
-
+# Create currency change data frame
 currency <- tibble::tibble(
   start    = c(1981, 1985.5, 1988.5, 1992.5, 1993.5),
   end      = c(1985.5, 1988.5, 1992.5, 1993.5, 1998),
   Currency =  c("Cruzerio", "Cruzado", "Cruzado Novo", "Cruzeiro\nReal", "Real")
 )
 
+# Read in CPI data
+url <- glue::glue("https://api.worldbank.org/v2/country/BRA/indicator/FP.CPI.TOTL?per_page=999&format=json")
+cpi <- jsonlite::fromJSON(url)[[2]]
+base_year <- cpi$date[cpi$value %in% 100]
+cpi <- cpi %>% select(year = date, value) %>% mutate(year = as.numeric(year))
 
-ggplot(wage) +
+# Create mean wage data from 1981 to 1998 for employees unified with the currency 
+wage_unif <- df %>%
+  filter(empstat == 1) %>%
+  filter(!is.na(wage_no_compen)) %>%
+  filter(year < 1999) %>%
+  mutate(
+    unified_wage = case_when(
+      year < 1986 ~ wage_no_compen/2750/1000/1000/1000,
+      between(year, 1986, 1988) ~ wage_no_compen/2750/1000/1000,
+      between(year, 1989, 1992) ~ wage_no_compen/2750/1000,
+      year == 1993 ~ wage_no_compen/2750,
+      year > 1993 ~ wage_no_compen
+    )) %>%
+  group_by(year) %>%
+  summarise(
+    mean_employee_wage = mean(unified_wage)  )
+
+# Apply CPI to unified wage data
+df_w_cpi <-
+  
+  left_join(wage_unif, cpi, by = "year") %>%
+  
+  # Deflate all wage info vars (all start with <<w_>>)
+  mutate(
+    mean_employee_wage = mean_employee_wage/(value/100),
+    # Add CPI base year
+    cpi_base = base_year,
+    # Add variable of interest (wage)
+    var = "Hourly wage"
+  )
+
+
+# Step 2 - Plot nominal values --------------------------------------------
+
+ggplot(wage_as_is) +
   
   geom_rect(
   aes(xmin = start, xmax = end, fill = Currency), 
@@ -49,55 +91,7 @@ ggplot(wage) +
 
 
 
-# Make new plot with unified currency -------------------------------------
-
-unified_df <- df %>%
-  mutate(
-    unified_wage = case_when(
-      year < 1986 ~ wage_no_compen/2750/1000/1000/1000,
-      between(year, 1986, 1988) ~ wage_no_compen/2750/1000/1000,
-      between(year, 1989, 1992) ~ wage_no_compen/2750/1000,
-      year == 1993 ~ wage_no_compen/2750,
-      year > 1993 ~ wage_no_compen
-    ))
-
-
-wage_unif <- 
-  unified_df %>% 
-  filter(empstat == 1) %>%
-  filter(!is.na(wage_no_compen)) %>%
-  filter(year < 1999) %>%
-  group_by(year) %>%
-  summarise(
-    mean_employee_wage = mean(unified_wage)  )
-
-# Read in CPI data
-url <- glue::glue("https://api.worldbank.org/v2/country/BRA/indicator/FP.CPI.TOTL?per_page=999&format=json")
-# Will load a list of 2, first is info, second is DF
-cpi <- jsonlite::fromJSON(url)[[2]]
-
-# As of December 2022, the values are centred on 2010 (= 100) but this may change
-# Hence we loaded above all years, find which year has 100
-base_year <- cpi$date[cpi$value %in% 100]
-
-# Keep only relevant info so we do not add too much in Step 4, 
-# rename date to year to match, make numeric
-cpi <- cpi %>% select(year = date, value) %>% mutate(year = as.numeric(year))
-
-# Merge it in
-df_w_cpi <-
-  
-  left_join(wage_unif, cpi, by = "year") %>%
-  
-  # Deflate all wage info vars (all start with <<w_>>)
-  mutate(
-    mean_employee_wage = mean_employee_wage/(value/100),
-    # Add CPI base year
-    cpi_base = base_year,
-    # Add variable of interest (wage)
-    var = "Hourly wage"
-  )
-
+# Step 3 - Plot unified, deflated values ----------------------------------
 
 ggplot(df_w_cpi) +
   
@@ -125,3 +119,34 @@ ggplot(df_w_cpi) +
   scale_x_continuous(breaks = seq(1981,1998,1)) + 
   guides(fill = "none") +
   labs(y = "Mean monthly employee wage", x = "Year") 
+
+
+# Make new plot with unified currency -------------------------------------
+
+unified_df <- df %>%
+  mutate(
+    unified_wage = case_when(
+      year < 1986 ~ wage_no_compen/2750/1000/1000/1000,
+      between(year, 1986, 1988) ~ wage_no_compen/2750/1000/1000,
+      between(year, 1989, 1992) ~ wage_no_compen/2750/1000,
+      year == 1993 ~ wage_no_compen/2750,
+      year > 1993 ~ wage_no_compen
+    ))
+
+
+wage_unif <- 
+  unified_df %>% 
+  filter(empstat == 1) %>%
+  filter(!is.na(wage_no_compen)) %>%
+  filter(year < 1999) %>%
+  group_by(year) %>%
+  summarise(
+    mean_employee_wage = mean(unified_wage)  )
+
+
+
+# Merge it in
+
+
+
+
