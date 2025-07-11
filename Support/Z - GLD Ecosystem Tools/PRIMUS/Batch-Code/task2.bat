743e4640c38f942e18fbdb16f158797243144007
Mario G <63501500+gronert-m@users.noreply.github.com>
2025-07-11 11:01:35 -0400

    Add comments

--- Support/Z - GLD Ecosystem Tools/PRIMUS/Batch-Code/task2.bat
+++ Support/Z - GLD Ecosystem Tools/PRIMUS/Batch-Code/task2.bat
@@ -1,2 +1,4 @@
 @echo off
+:: The line below first points to the location of Stata, then tells gives the command to execute a file using Stata
+:: Users should change the pointers to their specific locations
 "C:\Program Files\Stata18\StataMP-64.exe" /e do "C:\Users\wb510859\WBG\GLD - Current Contributors\510859_AS\PRIMUS\Program tasks\task2.do"
